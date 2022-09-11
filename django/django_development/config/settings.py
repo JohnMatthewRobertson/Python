1910c81d225983655d1e0fbde82f524160d70605
JohnMatthewRobertson <15859963+JohnMatthewRobertson@users.noreply.github.com>
2022-09-11 19:35:09 +0100

    add password reset and change basic email framework

--- django/django_development/config/settings.py
+++ django/django_development/config/settings.py
@@ -86,7 +86,20 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+DEFAULT_FROM_EMAIL = 'django_development@django_development.com'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+"""
+FOLLOWING TO BE USED WHEN SMTP EMAIL AVAILABLE
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("DJANGO_EMAIL_HOST_PASSWORD")
+EMAIL_HOST = env("DJANGO_EMAIL_HOST")
+EMAIL_PORT = env("DJANGO_EMAIL_PORT")
+EMAIL_USE_TLS = env("DJANGO_EMAIL_USE_TLS")
+"""
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
